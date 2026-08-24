3cd98c971398a3965831c915a7056fc8d6b1878e
tarrabmu-commits <tarrab.mu@gmail.com>
2026-08-24 10:36:02 +0300

    Add public intelligence automation plan executor skeleton

---
+++ src/main/java/io/rbvm/postgres/PublicIntelligenceAutomationPlanExecutor.java
@@ -0,0 +1,7 @@
+package io.rbvm.postgres;
+
+/** Executes explicit public-intelligence automation plans. */
+public final class PublicIntelligenceAutomationPlanExecutor {
+    private PublicIntelligenceAutomationPlanExecutor() {
+    }
+}
